110f935a706fa01f784473558538b8b4ccdfaa8d
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-29 23:11:39 +0300

    configuration loader

---
+++ utils/config_loader.py
@@ -0,0 +1,13 @@
+import os
+import yaml
+
+#function to load the configurations
+def load_config(config_path: str = None):
+    if config_path is None:
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        config_path = os.path.join(BASE_DIR, "config", "config.yaml")
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Config file not found: {config_path}")
+    
+    with open(config_path, 'r')  as file:
+        return yaml.safe_load(file)
